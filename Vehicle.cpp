8fe732ae98a30c31f00fc5781bc4952f57d0d1b0
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 19:37:37 -0400

    added vehicle.cpp to define displayInfo for class vehicle

---
+++ Vehicle.cpp
@@ -0,0 +1,12 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Inheritance
+
+#pragma once
+#include <iostream>
+#include "Vehicle.h"
+
+using namespace std;
+
+// define displayInfo function from vehicle class
