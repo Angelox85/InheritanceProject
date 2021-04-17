5bfa2ef3d36cb2b6f827b2fac5afb89fa38dbb47
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 19:42:13 -0400

    Create class truck

---
+++ Truck.h
@@ -0,0 +1,12 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Inheritance
+
+#pragma once
+#ifndef TRUCK_H
+#define TRUCK_H
+#include "Vehicle.h"
+#include <string>
+using namespace std;
+
