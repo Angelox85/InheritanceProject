b5a28234717e4db59f8435cc383f00b1f594f0d1
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 19:43:10 -0400

    truck is a type of vehicle that inherits members variables and functions fromthe class vehicle

--- Truck.h
+++ Truck.h
@@ -10,3 +10,36 @@
 #include <string>
 using namespace std;
 
+
+// Truck is a type of vehicle that inherits members variables
+//and functions from the class vehicle
+
+class Truck : public Vehicle
+{
+private:
+	int capacity = 0;
+public:
+	// defaul constructor
+	Truck() : Vehicle()
+	{
+		int capacity = 0;
+	}
+
+	// constructor 2
+	Truck(string vehicleManf, int vehicleYear, int vehicleCapacity)
+		: Vehicle(vehicleManf, vehicleYear)
+	{
+		capacity = vehicleCapacity;
+	}
+
+	// Accesor for capacity attribute
+	int getCapacity()
+	{
+		return capacity;
+	}
+
+	// display info
+	void displayInfo(); // define in truck.cpp
+
+};
+#endif
