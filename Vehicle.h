3885f198ad7be244b987aa5e5ec55e2f103b44b1
Angel Hernandez Sierra <angel.hernandezsierr@sinclair.edu>
2021-04-17 19:31:29 -0400

    Add project files.

---
+++ Vehicle.h
@@ -0,0 +1,12 @@
+// Name : Angel E Hernandez
+// Date : April 17
+// CIS 1202.800
+// Project name : Inheritance
+
+#pragma once
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include <string>
+using namespace std;
+
+
